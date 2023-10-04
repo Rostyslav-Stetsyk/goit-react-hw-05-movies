1482cec68361d60e2cb2d6551c6e2986a0cba53a
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2023-10-04 22:13:11 +0300

    hover+change design

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -2,6 +2,8 @@ import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
 export const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: center;
   position: fixed;
   z-index: 1;
   width: 100vw;
@@ -12,17 +14,35 @@ export const HeaderStyled = styled.header`
 
 export const HeaderContainer = styled.div`
   display: flex;
+
   gap: 30px;
-  align-items: center;
   max-width: 1660px;
+  width: 100%;
 `;
 
 export const HomeLink = styled(Link)`
   display: flex;
+  position: relative;
+  align-items: center;
   font-weight: 500;
   font-size: 24px;
-  color: #7183f2;
+  color: #7f8ff4;
   text-decoration: none;
+  transition: color 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  ::before {
+    content: '';
+    width: 142px;
+    height: 4px;
+    background-color: #7f8ff4;
+    border-radius: 2px;
+    position: absolute;
+    bottom: 0;
+    transform: scaleX(0);
+    transition: transform 500ms cubic-bezier(0.075, 0.82, 0.165, 1),
+      color 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
   ::after {
     content: '';
     display: block;
@@ -30,20 +50,34 @@ export const HomeLink = styled(Link)`
     width: 0;
     height: 0;
     border-top: 12px solid transparent;
-    border-left: 21px solid #7183f2;
+    border-left: 21px solid #7f8ff4;
     border-bottom: 12px solid transparent;
+    transition: transform 500ms cubic-bezier(0.075, 0.82, 0.165, 1),
+      border-left 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  :hover {
+    color: #7183f2;
+    ::after {
+      transform: rotate(120deg);
+      border-left: 21px solid #7183f2;
+    }
+
+    ::before {
+      transform: scaleX(1);
+      color: #7183f2;
+    }
   }
 `;
 
 export const SearchLink = styled(Link)`
   display: block;
   font-weight: 500;
-  font-size: 24px;
+  font-size: 18px;
   color: #7f8ff4;
   text-decoration: none;
-  font: inherit;
   border: 0;
-  transition: all 200ms ease-in;
+  transition: color 200ms cubic-bezier(0.075, 0.82, 0.165, 1);
   cursor: pointer;
   padding: 12px 36px;
 
@@ -52,7 +86,7 @@ export const SearchLink = styled(Link)`
   }
 
   :active {
-    color: #7f8ff4;
+    color: #7183f2;
   }
 `;
 
--- src/components/MovieElement/MovieElement.styled.jsx
+++ src/components/MovieElement/MovieElement.styled.jsx
@@ -1,19 +1,31 @@
 import styled from '@emotion/styled';
 
 export const ListMovieEl = styled.li`
+  position: relative;
   width: 300px;
-  border-radius: 12px;
   overflow: hidden;
   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  transition: transform 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  :hover {
+    transform: scale(1.02);
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  }
 `;
 
 export const MovieTitle = styled.h2`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  background-color: #7182f2bb;
   font-size: large;
   font-weight: 600;
   padding: 16px;
   min-height: 80px;
   line-height: 1.3;
   text-decoration-line: none;
-  color: #7183f2;
+  color: white;
+  text-align: center;
+  text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.3);
 `;
